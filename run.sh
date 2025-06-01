d267f20fa7323144b0f354f42be814ef5a8667df
Ladbaby <1390265970@qq.com>
2025-06-01 16:45:18 +0800

    update script

--- run.sh
+++ run.sh
@@ -1,48 +1,30 @@
 #!/bin/bash
 
-# for dataset_name in "ECL" "ETTh1" "ETTm1" "ILI" "Traffic" "Weather"; do
-for dataset_name in "P12" "MIMIC_III" "MIMIC_IV" "USHCN" "HumanActivity"; do
-    # sh scripts/Ada_MSHyper/$dataset_name.sh
-    # sh scripts/Autoformer/$dataset_name.sh
-    # sh scripts/BigST/$dataset_name.sh
-    # sh scripts/Crossformer/$dataset_name.sh
-    # sh scripts/CRU/$dataset_name.sh
-    # sh scripts/DLinear/$dataset_name.sh
-    # sh scripts/ETSformer/$dataset_name.sh
-    # sh scripts/FEDformer/$dataset_name.sh
-    # sh scripts/FiLM/$dataset_name.sh
-    # sh scripts/FourierGNN/$dataset_name.sh
-    # sh scripts/FreTS/$dataset_name.sh
-    # sh scripts/GNeuralFlow/$dataset_name.sh
-    # sh scripts/GraFITi/$dataset_name.sh
-    # sh scripts/GRU_D/$dataset_name.sh
-    # sh scripts/Hi-Patch/$dataset_name.sh
-    # sh scripts/higp/$dataset_name.sh
-    # sh scripts/HyperIMTS/$dataset_name.sh
-    # sh scripts/Informer/$dataset_name.sh
-    # sh scripts/iTransformer/$dataset_name.sh
-    # sh scripts/Koopa/$dataset_name.sh
-    # sh scripts/Latent_ODE/$dataset_name.sh
-    # sh scripts/Leddam/$dataset_name.sh
-    # sh scripts/LightTS/$dataset_name.sh
-    # sh scripts/MambaSimple/$dataset_name.sh
-    # sh scripts/MICN/$dataset_name.sh
-    # sh scripts/MOIRAI/$dataset_name.sh
-    # sh scripts/mTAN/$dataset_name.sh
-    # sh scripts/NeuralFlows/$dataset_name.sh
-    # sh scripts/Nonstationary_Transformer/$dataset_name.sh
-    # sh scripts/PatchTST/$dataset_name.sh;
-    # sh scripts/PrimeNet/$dataset_name.sh;
-    # sh scripts/Pyraformer/$dataset_name.sh;
-    # sh scripts/Raindrop/$dataset_name.sh;
-    # sh scripts/Reformer/$dataset_name.sh;
-    # sh scripts/SeFT/$dataset_name.sh;
-    # sh scripts/SegRNN/$dataset_name.sh;
-    # sh scripts/TiDE/$dataset_name.sh;
-    # sh scripts/TimeMixer/$dataset_name.sh;
-    # sh scripts/TimesNet/$dataset_name.sh
-    # sh scripts/tPatchGNN/$dataset_name.sh
-    # sh scripts/Transformer/$dataset_name.sh
-    # sh scripts/TSMixer/$dataset_name.sh
-    # sh scripts/Warpformer/$dataset_name.sh
-done
+# sh scripts/Linear/OpenMIC.sh
+# sh scripts/Autoformer/OpenMIC.sh
+# sh scripts/Crossformer/OpenMIC.sh
+# sh scripts/DLinear/OpenMIC.sh
+# sh scripts/FEDformer/OpenMIC.sh
+# sh scripts/FiLM/OpenMIC.sh
+# sh scripts/FreTS/OpenMIC.sh
+# sh scripts/GRU_D/OpenMIC.sh
+# sh scripts/Hi_Patch/OpenMIC.sh
+# sh scripts/Informer/OpenMIC.sh
+# sh scripts/iTransformer/OpenMIC.sh
+# sh scripts/Leddam/OpenMIC.sh
+# sh scripts/LightTS/OpenMIC.sh
+# sh scripts/MambaSimple/OpenMIC.sh
+# sh scripts/MICN/OpenMIC.sh
+# sh scripts/mTAN/OpenMIC.sh
+# sh scripts/Nonstationary_Transformer/OpenMIC.sh
+# sh scripts/PatchTST/OpenMIC.sh
+# sh scripts/PrimeNet/OpenMIC.sh
+# sh scripts/Pyraformer/OpenMIC.sh
+# sh scripts/Raindrop/OpenMIC.sh
+# sh scripts/Reformer/OpenMIC.sh
+# sh scripts/SeFT/OpenMIC.sh
+# sh scripts/SegRNN/OpenMIC.sh
+# sh scripts/TiDE/OpenMIC.sh
+# sh scripts/TimesNet/OpenMIC.sh
+# sh scripts/Transformer/OpenMIC.sh
+# sh scripts/TSMixer/OpenMIC.sh
